d2baeba24e2695b6afb5d2f1a3398e5a56e05730
ssh-void <168687039+ssh-void@users.noreply.github.com>
2025-02-08 22:21:35 +0100

    Update src.sh

--- src/src.sh
+++ src/src.sh
@@ -3,7 +3,7 @@ cd $HOME/my-suckless-tools/src/stt/ && sudo make -s clean install &> /dev/null
 cd $HOME/my-suckless-tools/src/slstatus && sudo make -s clean install &> /dev/null
 cd $HOME/my-suckless-tools/src/dmenu && sudo make -s clean install &> /dev/null
 # rm *.o and config.h
-cd $HOME/my-suckless-tools/src/dwm/ ; rm -rvfi *.o config.h 
-cd $HOME/my-suckless-tools/src/stt/ ; rm -rvfi *.o config.h 
-cd $HOME/my-suckless-tools/src/slstatus ; rm -rvfi *.o config.h 
-cd $HOME/my-suckless-tools/src/dmenu ; rm -rvfi *.o config.h 
+cd $HOME/my-suckless-tools/src/dwm/ ; rm -rvf *.o config.h 
+cd $HOME/my-suckless-tools/src/stt/ ; rm -rvf *.o config.h 
+cd $HOME/my-suckless-tools/src/slstatus ; rm -rvf *.o config.h 
+cd $HOME/my-suckless-tools/src/dmenu ; rm -rvf *.o config.h 
