46526ae60a654441177f5bf3d092b3e42f6bfdb1
ssh-void <168687039+ssh-void@users.noreply.github.com>
2025-02-10 13:15:25 +0100

    Update config.def.h

--- src/slstatus/config.def.h
+++ src/slstatus/config.def.h
@@ -1,83 +1,113 @@
-/* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
+/* appearance */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const int splitstatus        = 0;        /* 1 for split status items */
+static const char *splitdelim       = ";";       /* Character used for separating status */
+static const char *fonts[]          = {"Ubuntu Mono:size=10:weight=SemiBold:pixelsize=22:antialias=true:autohint=true",
+                                       "JoyPixels:style=Bold:pixelsize=15:antialias=true:autohint=true"};
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#2067B0";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#ffffff";
+static const char col_cyan[]        = "#005577";
+static const char evening[]         = "#333333";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray4, evening, evening },
+	[SchemeSel]  = { evening, col_gray3, col_gray2},
+};
+/* tagging */
+static const char *tags[] = { "f", "b", "t", "v", "s", "6", "7", "8", "q" };
+static const Rule rules[] = {
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 */
+	/* class                   instance                   title          tags mask     iscentered   isfloating   monitor */
+	{ "st",                    "scpad",                    NULL,             0,            1,           1,           -1 },
+	{ "Thunar",                "thunar",                   NULL,             0,            1,           1,           -1 },
+	{ "qBittorrent",           "qbittorrent",              NULL,             0,            1,           1,           -1 },
+	{ "Xfce4-appfinder",       "xfce4-appfinder",          NULL,             0,            1,           1,           -1 },
+	{ "pavucontrol",           "pavucontrol",              NULL,             0,            1,           1,           -1 },
+	{ "Blueman-manager",       "blueman-manager",          NULL,             0,            1,           1,           -1 },
+	{ "Zathura",               "org.pwmt.zathura",         NULL,             0,            1,           1,           -1 },
+	{ "Rhythmbox",             "rhythmbox",                NULL,             0,            1,           1,           -1 },
 
-/* interval between updates (in ms) */
-const unsigned int interval = 1000;
-
-/* text to show if no value can be retrieved */
-static const char unknown_str[] = "n/a";
-
-/* maximum output string length */
-#define MAXLEN 2048
-
-/*
- * function            description                     argument (example)
- *
- * battery_perc        battery percentage              battery name (BAT0)
- *                                                     NULL on OpenBSD/FreeBSD
- * battery_remaining   battery remaining HH:MM         battery name (BAT0)
- *                                                     NULL on OpenBSD/FreeBSD
- * battery_state       battery charging state          battery name (BAT0)
- *                                                     NULL on OpenBSD/FreeBSD
- * cat                 read arbitrary file             path
- * cpu_freq            cpu frequency in MHz            NULL
- * cpu_perc            cpu usage in percent            NULL
- * datetime            date and time                   format string (%F %T)
- * disk_free           free disk space in GB           mountpoint path (/)
- * disk_perc           disk usage in percent           mountpoint path (/)
- * disk_total          total disk space in GB          mountpoint path (/)
- * disk_used           used disk space in GB           mountpoint path (/)
- * entropy             available entropy               NULL
- * gid                 GID of current user             NULL
- * hostname            hostname                        NULL
- * ipv4                IPv4 address                    interface name (eth0)
- * ipv6                IPv6 address                    interface name (eth0)
- * kernel_release      `uname -r`                      NULL
- * keyboard_indicators caps/num lock indicators        format string (c?n?)
- *                                                     see keyboard_indicators.c
- * keymap              layout (variant) of current     NULL
- *                     keymap
- * load_avg            load average                    NULL
- * netspeed_rx         receive network speed           interface name (wlan0)
- * netspeed_tx         transfer network speed          interface name (wlan0)
- * num_files           number of files in a directory  path
- *                                                     (/home/foo/Inbox/cur)
- * ram_free            free memory in GB               NULL
- * ram_perc            memory usage in percent         NULL
- * ram_total           total memory size in GB         NULL
- * ram_used            used memory in GB               NULL
- * run_command         custom shell command            command (echo foo)
- * swap_free           free swap in GB                 NULL
- * swap_perc           swap usage in percent           NULL
- * swap_total          total swap size in GB           NULL
- * swap_used           used swap in GB                 NULL
- * temp                temperature in degree celsius   sensor file
- *                                                     (/sys/class/thermal/...)
- *                                                     NULL on OpenBSD
- *                                                     thermal zone on FreeBSD
- *                                                     (tz0, tz1, etc.)
- * uid                 UID of current user             NULL
- * uptime              system uptime                   NULL
- * username            username of current user        NULL
- * alsa_master_vol     ALSA Master device volume       NULL
- * vol_perc            OSS/ALSA volume in percent      mixer file (/dev/mixer)
- *                                                     NULL on OpenBSD/FreeBSD
- * wifi_essid          WiFi ESSID                      interface name (wlan0)
- * wifi_perc           WiFi signal in percent          interface name (wlan0)
- */
-static const struct arg args[] = {
-    { datetime,"%s;","%A|%F|%r"},
-//  { run_command ,"brt:%s%% ","xbacklight -get"}
-//  { alsa_master_vol ,"vol:%s|",NULL},
-    { run_command ,"vol:%s|","pamixer --get-volume-human"}, 
-//  { run_command ,"xbps:%s|","doas xbps-query -l | wc -l"},
-//  { netspeed_tx, "up %s%|", "wlo1"  },
-    { cpu_perc,"cpu:%s%% ",NULL },
-    { run_command, "%s|","sensors coretemp-isa-0000 | awk '/+(.*)/ {print $3}' | awk 'NR==2'"},
-//  { ram_free,"ram:%s|",NULL},
-    { ram_perc,"ram:%s%%|",NULL},
-//  { disk_free,"/: %s","/"},
-    { disk_perc,"/:%s%%","/"},
-//  { disk_perc, "/HOME: %s%%|",  "/home"  },
-//  { battery_perc, "bat:%s%%|", "BAT1"}, /*hp bat1*/
-//  { keymap, "%s", NULL},
-    }; 
+};
+/* layout(s) */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const Layout layouts[] = {
+	/* symbol     arrange function */
+	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[M]",      monocle },
+};
+/* key definitions */
+#define MODKEY Mod4Mask
+#define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+/* commands */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-i", "-fn", "Ubuntu Font:style=Medium:size=15", "-nb", "#333333", "-nf", "#ffffff", "-sb", "#c61169", "-sf", "#f9f102", NULL };
+static const char *termcmd[]  = { "st",NULL };
+static const Key keys[] = {
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+        { MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} }, /* bat off or on */
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_l,      pushup,         {0} },
+	{ MODKEY|ShiftMask,             XK_k,      pushdown,       {0} },
+//	{ MODKEY|ShiftMask,             XK_i,      incnmaster,     {.i = +1 } }, /*vers le master  */
+//	{ MODKEY|ShiftMask,             XK_o,      incnmaster,     {.i = -1 } }, /* l*/
+	{ MODKEY,                       XK_j,      setmfact,       {.f = -0.03} },
+	{ MODKEY,                       XK_m,      setmfact,       {.f = +0.03} },
+	{ MODKEY|ShiftMask,                       XK_Return, zoom,           {0} }, /*changer win avec le master*/
+	{ MODKEY,                       XK_Tab,    view,           {0} }, 
+	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
+//	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[0]} },
+//	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
+//	{ MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[2]} }, /* monocle*/
+//  { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },/*switch btwn mounitor*/
+	{ MODKEY,                       XK_semicolon, focusmon,    {.i = +1 } },/*switch btwn mounitor*/
+//  { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },/*envery un fentre  btwn mounitor*/
+	{ MODKEY|ShiftMask,             XK_semicolon, tagmon,      {.i = +1 } },/*envery un fentre  btwn mounitor*/
+	{ MODKEY|ControlMask|ShiftMask, XK_q,           quit,      {0} },
+	TAGKEYS(                        XK_ampersand,              0)
+	TAGKEYS(                        XK_eacute,                 1)
+	TAGKEYS(                        XK_quotedbl,               2)
+	TAGKEYS(                        XK_apostrophe,             3)
+	TAGKEYS(                        XK_parenleft,              4)
+	TAGKEYS(                        XK_minus,                  5)
+	TAGKEYS(                        XK_egrave,                 6)
+	TAGKEYS(                        XK_underscore,             7)
+	TAGKEYS(                        XK_ccedilla,               8)
+};
+/* button definitions */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+static const Button buttons[] = {
+	/* click                event mask      button          function        argument */
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+//	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+//	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+//	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+//	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkTagBar,            0,              Button1,        view,           {0} },
+//	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+//	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+//	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+};
